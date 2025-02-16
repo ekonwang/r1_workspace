3e55c090e832dbe021ccf7ebeb179692c8f022b8
ekon <g1547246193@gmail.com>
2025-02-16 17:40:56 +0800

    update

---
+++ workspace/cmd/cmd_set.sh
@@ -0,0 +1,26 @@
+# --- cmd tools --- #
+
+# git add & commit & push
+git_all() {
+	git add .
+	git commit -m "$1"
+	git push
+}
+
+gpul() {
+	git pull origin dev
+}
+
+ifs(){
+	# cd to mcts workspace and then innitialize file structure
+	echo 'initialize directories for '$1
+	mkdir $1/.temp
+	mkdir $1/.log
+}
+
+gset() {
+	if [ -f '/inspire' ]; then
+		# only reset on inspire cluster
+		git reset --hard HEAD
+	fi
+}
---
+++ workspace/cmd/init.sh
@@ -0,0 +1,3 @@
+pip install pyautogen==0.2.26
+pip install 'pyautogen[jupyter-executor]'
+pip install Pillow joblib matplotlib opencv-python numpy gradio gradio_client networkx scipy datasets
