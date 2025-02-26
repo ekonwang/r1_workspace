6747335e5755bc30f7d5769694505bc4ea07b04c
ekon <g1547246193@gmail.com>
2025-02-26 22:34:57 +0800

    update

--- workspace/scripts/demo_tikz_to_plt.py
+++ workspace/scripts/demo_tikz_to_plt.py
@@ -6,11 +6,11 @@ from openai import OpenAI
 import json
 import shutil
 
-from utils_chat import chat_gpt4o
-from utils_prompt import TIKZ_CONVERT_EXAMPLES
-from utils_execution import CodeExecutor
-from utils_parse import Parser
-from demo_jpeg_convert import convert_jpeg_to_png
+# from utils_chat import chat_gpt4o
+# from utils_prompt import TIKZ_CONVERT_EXAMPLES
+# from utils_execution import CodeExecutor
+# from utils_parse import Parser
+# from demo_jpeg_convert import convert_jpeg_to_png
 
 sys.path.append('./workspace')
 from utils import *
@@ -41,8 +41,8 @@ def process_data(d, working_dir):
     image_path = d['image_path']
     tikz_code = d['tikz']
 
-    parser = Parser()
-    executor = CodeExecutor(working_dir)
+    # parser = Parser()
+    # executor = CodeExecutor(working_dir)
     image_path = TEMP_ROOT + image_path
 
     # move image to working_dir
@@ -163,56 +163,6 @@ Please generate the Python code according to the requirements strictly and the t
     print(PROMPT)
     # exit()
     return
-
-    content, messages = chat_gpt4o(PROMPT)
-
-    # print(content)
-    # print('-' * 50 + '\n' * 3)
-
-    for i in range(2):
-        if 'TERMINATE' in content:
-            break 
-
-        result = parser.parse(content)
-        assert result['status'], f"{content}"
-        exec_result = executor.execute(result['content'])
-        
-        if exec_result[0] == 0:
-            # OK 
-            img_str = exec_result[1]
-            prompt = f"""
-            OBSERVATION: Execution success. The output is as follows:
-            {img_str}
-            - The python code must cover all the information that appear in the figure
-            - The rendered output should geometrically equivalent to the ## TARGET GEOMETRY ##, and avoid to cause obvious confusion for adding, losing or twisting information (including quite wrong length, radius or positions, etc)
-            - Pay attention to the shape of the geometry, the output figure should be strictly the same as the ## TARGET GEOMETRY ##.
-            - Any element is shown completely in the figure, else adjust x lim and y lim for better presentation.
-            **please compare the output with ## TARGET GEOMETRY ## carefully**
-            - If the output satisfies, reply with TERMINATE
-            - else, reply GENERATE and generate the fixed code.
-            """
-        else:
-            # ERROR 
-            prompt = f"""
-            OBSERVATION: Execution error. Error message:
-            {exec_result[1]}
-            Please fix the error and generate the fixed code, in the next THOUGHT and ACTION.
-            """
-        
-        # print(prompt)
-        # print('-' * 50 + '\n' * 3)
-
-        content, messages = chat_gpt4o(prompt, messages)
-
-        # print(content)
-        # print('-' * 50 + '\n' * 3)
-    json.dump(messages, open(os.path.join(working_dir, 'generate_oai.json'), 'w'), indent=4)
-
-    if 'TERMINATE' in content:
-        return True, result['content']
-    else:
-        return False, None
-
 
 
 if __name__ == "__main__":
