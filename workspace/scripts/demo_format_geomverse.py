f76d47de07d2e4f5499863315dbede579ba84e55
ekon <g1547246193@gmail.com>
2025-03-26 17:56:55 +0800

    update

--- workspace/scripts/demo_format_geomverse.py
+++ workspace/scripts/demo_format_geomverse.py
@@ -1,10 +1,13 @@
 import utils
+import argparse
 
 DATA = '.temp/datasets/GeomVerse/TRAIN/TRAIN_MIX/data.jsonl'
 OUTPUT_DATA = '.temp/datasets/GeomVerse/TRAIN/TRAIN_MIX/grpo_conversations.jsonl'
-jsonl_data = utils.load_jsonl(DATA)
 QUESTION_TEMPLATE = "{Question}  Output the thinking process in <think> </think> and final answer (number) in <answer> </answer> tags.\n\n\n"\
         "Here is the tikz code for the geometry problem:```\n{tikz}\n```"
+EDIT_TEMPLATE = "{Question}  Output the thinking process in <think> </think> and final answer (number) in <answer> </answer> tags.\n\n\n"\
+        "Here is the tikz code for the geometry problem:```\n{tikz}\n```"\
+        "Optionally, consider to edit the tikz code to construct auxiliary lines, which should be marked with <auxiliary> </auxiliary> tags."
 
 def format_data(data):
     tikz_code = data['tikz']
@@ -24,11 +27,20 @@ def format_data(data):
         'answer': answer,
     }
 
-new_data = []
-for data in utils.mk_pbar(jsonl_data):
-    formatted_data = format_data(data)
-    new_data.append(formatted_data)
 
-utils.save_jsonl(new_data, OUTPUT_DATA)
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--data', type=str, default=DATA)
+    parser.add_argument('--output', type=str, default=OUTPUT_DATA)
+    return parser.parse_args()
 
-    
+
+if __name__ == '__main__':
+    args = parse_args()
+    new_data = []
+    jsonl_data = utils.load_jsonl(args.data)
+    for data in utils.mk_pbar(jsonl_data):
+        formatted_data = format_data(data)
+        new_data.append(formatted_data)
+
+    utils.save_jsonl(new_data, args.output)
