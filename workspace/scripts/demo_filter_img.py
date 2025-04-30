f92a8dac62b2de30ee84fb191063bbafd6e4c68b
ekon <g1547246193@gmail.com>
2025-04-30 13:11:35 +0800

    update

--- workspace/scripts/demo_filter_img.py
+++ workspace/scripts/demo_filter_img.py
@@ -20,24 +20,26 @@ def load_sub_dir(img_dir):
     return sub_paths
             
 
-def scavenger(img_dir, filter_img_dir):
+def scavenger(img_dir, filter_img_dir, jsonl_name = "gps-mathvista-geometry-v2.jsonl"):
     sub_paths = load_sub_dir(img_dir)
     filter_sub_paths = load_sub_dir(filter_img_dir)
 
-    jsonl_path = os.path.join(img_dir, "gps-mathvista-geometry-v2.jsonl")
+    jsonl_path = os.path.join(img_dir, jsonl_name)
     jsonl_list = load_jsonl(jsonl_path)
     new_jsonl = []
     assert len(jsonl_list) == len(sub_paths)
 
     for i, sub_name in enumerate(filter_sub_paths):
         if sub_name in sub_paths:
-            new_jsonl.append(jsonl_list[i])
+            new_data = jsonl_list[i]
+            new_data['image_path'] = sub_name
+            new_jsonl.append(new_data)
         else:
             print(f"sub_name: {sub_name} not in sub_paths")
     
-    target_jsonl_path = os.path.join(filter_img_dir, "filter-gps-mathvista-geometry-v2.jsonl")
-    save_jsonl(target_jsonl_path, new_jsonl)
+    target_jsonl_path = os.path.join(filter_img_dir, jsonl_name)
+    save_jsonl(new_jsonl, target_jsonl_path)
 
 if __name__ == "__main__":
-    scavenger("/inspire/hdd/ws-f4d69b29-e0a5-44e6-bd92-acf4de9990f0/public-project/wangyikun-240108120104/r1_workspace/.temp/datasets/gps-mathvista-geometry-v2", "/inspire/hdd/ws-f4d69b29-e0a5-44e6-bd92-acf4de9990f0/public-project/wangyikun-240108120104/r1_workspace/.temp/datasets/filter-gps-mathvista-geometry-v2")
-    scavenger("/inspire/hdd/ws-f4d69b29-e0a5-44e6-bd92-acf4de9990f0/public-project/wangyikun-240108120104/r1_workspace/.temp/datasets/gps-olympiad-bench", "/inspire/hdd/ws-f4d69b29-e0a5-44e6-bd92-acf4de9990f0/public-project/wangyikun-240108120104/r1_workspace/.temp/datasets/filter-gps-olympiad-bench")
+    scavenger(".temp/datasets/gps-mathvista-geometry-v2", ".temp/datasets/filter-gps-mathvista-geometry-v2", "gps-mathvista-geometry-v2.jsonl")
+    scavenger(".temp/datasets/gps-olympiad-bench", ".temp/datasets/filter-gps-olympiad-bench", "gps-olympiad-bench.jsonl")
